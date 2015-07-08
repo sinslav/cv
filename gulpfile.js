5dc59d9cfe5d846049e4a3f03c7beb2a10489afe
sinslav <sinslav@gmail.com>
2015-07-08 18:24:07 +0300

    start layout creating

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-	jade = require('gulp-jade');
+jade = require('gulp-jade'),
+sass = require('gulp-ruby-sass');
+gulp = require('gulp'),
+cssmin = require('gulp-cssmin'),
+rename = require('gulp-rename');
 
-// gulp.task('default', function() {
-	
-// });
+gulp.task('default', ['jade', 'sass', 'cssmin', 'watch']);
 
 gulp.task('jade', function  () {
 	gulp.src(['./src/jade/*.jade', '!./src/jade/_*.jade'])
@@ -11,4 +13,26 @@ gulp.task('jade', function  () {
 		pretty: true
 	}))
 	.pipe(gulp.dest('./build/'))
+});
+
+
+gulp.task('sass', function () {
+    return sass('src/sass/')
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(gulp.dest('src/css/'));
+});
+ 
+gulp.task('cssmin', function () {
+    gulp.src('src/css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/assets/css/'));
+});
+
+gulp.task('watch', function  () {
+	gulp.watch('./src/jade/*.jade', ['jade']);
+    gulp.watch('./src/sass/*.scss', ['sass']);
+	gulp.watch('src/css/*.css', ['cssmin']);
 });
